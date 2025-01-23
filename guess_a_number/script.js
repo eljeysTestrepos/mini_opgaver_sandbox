7880133b75ddb43c6ad2f1b4b2ceacd79ae89308
eljeys <eljeys1@gmail.com>
2025-01-23 13:07:51 +0100

    finished randomNumber Generator

--- guess_a_number/script.js
+++ guess_a_number/script.js
@@ -1,3 +1,35 @@
 "use strict";
-const inputField = document.querySelector("#input_filed");
-const subBtn = document.querySelector("#sub_btn");
+const ranNum = Math.floor(Math.random() * 100) + 1;
+let myGuess = 0;
+document.querySelector("#sub_btn").addEventListener("click", guessNumber);
+
+function guessNumber() {
+  myGuess++;
+  const inputField = document.querySelector("#input_filed").value;
+
+  if (ranNum > inputField) {
+    alert(`Nice try! ${inputField} is to low. Try again`);
+  } else if (ranNum < inputField) {
+    alert(`Nice try! ${inputField} is to high. Try again`);
+  } else {
+    alert(
+      `${inputField} is correct. You used ${myGuess} guess. Feel free to try a new round!`
+    );
+    location.reload();
+  }
+}
+
+// function reset() {
+//   let newNumber = ranNum;
+//   return newNumber;
+// inputField.value === ranNum
+// }
+
+// myGuess.push(inputField.value);
+// if (ranNum === inputField.value) {
+//   alert(`${ranNum} er rigtigt!! ${myGuess.length}`);
+// } else if (inputField.value < ranNum) {
+//   alert(`Øv ${inputField.value} er for lavt. Prøv igen`);
+// } else {
+//   alert(`${inputField.value} er for højt. Prøv igen.`);
+// }
