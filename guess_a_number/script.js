58b0827525265261c185ff41495060e6258afa82
eljeys <eljeys1@gmail.com>
2025-01-24 16:19:22 +0100

    modals working

--- guess_a_number/script.js
+++ guess_a_number/script.js
@@ -1,35 +1,77 @@
 "use strict";
 const ranNum = Math.floor(Math.random() * 100) + 1;
+
 let myGuess = 0;
 document.querySelector("#sub_btn").addEventListener("click", guessNumber);
-
+console.log(ranNum);
 function guessNumber() {
   myGuess++;
   const inputField = document.querySelector("#input_filed").value;
 
   if (ranNum > inputField) {
-    alert(`Nice try! ${inputField} is to low. Try again`);
+    return showAlert();
+    // alert(`Nice try! ${inputField} is to low. Try again`);
   } else if (ranNum < inputField) {
-    alert(`Nice try! ${inputField} is to high. Try again`);
+    return showAlert();
+    // alert(`Nice try! ${inputField} is to high. Try again`);
   } else {
-    alert(
-      `${inputField} is correct. You used ${myGuess} guess. Feel free to try a new round!`
-    );
-    location.reload();
+    return showAlert();
+    // alert();
+    // `${inputField} is correct. You used ${myGuess} guess. Feel free to try a new round!`
+    // location.reload();
   }
 }
 
-// function reset() {
-//   let newNumber = ranNum;
-//   return newNumber;
-// inputField.value === ranNum
+//activate our alert inputs to the user
+function showAlert() {
+  document.querySelector("#custome_alert").style.display = "flex";
+  let article = document.querySelector("article");
+  const inputField = document.querySelector("#input_filed").value;
+  if (ranNum > inputField) {
+    article.innerHTML = `Nice try! ${inputField} Is to low. Try again!`;
+  } else if (ranNum < inputField) {
+    article.innerHTML = `Nice try! ${inputField} Is to high. Try again!`;
+  } else {
+    article.innerHTML = `${inputField} Is to correct. You used ${myGuess} guess. <br> Feel free to try a new round!`;
+  }
+}
+
+function closeAlert() {
+  document.querySelector("#custome_alert").style.display = "none";
+}
+function rest() {
+  console.log("New number generate");
+  location.reload();
+}
+// if (ranNum > inputField) {
+//   alert(`Nice try! ${inputField} is to low. Try again`);
+// } else if (ranNum < inputField) {
+//   alert(`Nice try! ${inputField} is to high. Try again`);
+// } else {
+//   // alert(
+//   //   `${inputField} is correct. You used ${myGuess} guess. Feel free to try a new round!`
+//   // );
+//   // location.reload();
+//   return showAlert();
+// }
 // }
 
-// myGuess.push(inputField.value);
-// if (ranNum === inputField.value) {
-//   alert(`${ranNum} er rigtigt!! ${myGuess.length}`);
-// } else if (inputField.value < ranNum) {
-//   alert(`Øv ${inputField.value} er for lavt. Prøv igen`);
-// } else {
-//   alert(`${inputField.value} er for højt. Prøv igen.`);
+// function showAlert() {
+//   document.querySelector("#custome_alert").style.display = "flex";
+//   let article = document.querySelector("article").textContent;
+//   const inputField = document.querySelector("#input_filed").value;
+//   if (ranNum > inputField) {
+//     article = document.querySelector(
+//       "article"
+//     ).textContent = `Nice try! ${inputField} Is to low. Try again!`;
+//   } else if (ranNum < inputField) {
+//     article = document.querySelector(
+//       "article"
+//     ).textContent = `Nice try! ${inputField} Is to high. Try again!`;
+//   } else {
+//     article = document.querySelector(
+//       "article"
+//     ).textContent = `${inputField} Is to correct. You used ${myGuess} guess. Feel free to try a new round!`;
+//     closeAlert();
+//   }
 // }
